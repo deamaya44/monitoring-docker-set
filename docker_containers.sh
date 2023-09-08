b3d6af489c6cdadd737e6f29ee3c8335285d49a5
David Amaya <amayadavid44@gmail.com>
2023-09-07 21:34:29 -0500

    first commit

---
+++ docker_containers.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# #Prometheus directory bind-mount volume config
+# docker run -d \
+#     -p 9090:9090 \
+#     --name prom \
+#     --network monitoring \
+#     -v config-prom:/etc/prometheus \
+#     -v prometheus-data:/prometheus \
+#     prom/prometheus
+
+#Prometheus file bind-volume config
+docker run -d \
+    -p 9090:9090 \
+    --name prom \
+    --network monitoring \
+    -v ./prom/config/prometheus.yml:/etc/prometheus/prometheus.yml \
+    -v ./prom/data/prometheus-data:/prometheus \
+    prom/prometheus
+
+#Grafana config
+docker run -d \
+    -p 3000:3000 \
+    --name grafana \
+    --network monitoring \
+    -v ./grafana/data:/var/lib/grafana \
+    grafana/grafana
+    # --user "$(id -u)" 
+
+#Alert Manager config
+docker run -d \
+    -p 9093:9093 \
+    --name alertman \
+    --network monitoring \
+    -v ./alertmanager/config:/config \
+    -v ./alertmanager/data:/data \
+    prom/alertmanager \
+    --config.file=/config/alertmanager.yml --log.level=debug
+    # --user "$(id -u)" 
---
+++ firs-time.sh
@@ -0,0 +1,4 @@
+mkdir -p prom/{config,data}
+mkdir -p grafana/data
+mkdir -p alertmanager/{config,data}
+bash docker-container.sh
