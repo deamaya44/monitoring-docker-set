297d7694eb35ee5fd1182c89a2d297bc2255f908
David Amaya <amayadavid44@gmail.com>
2023-09-07 21:56:44 -0500

    Update firstime script

--- firs-time.sh
+++ firs-time.sh
@@ -1,4 +1,8 @@
+#Create necessary dirs
 mkdir -p prom/{config,data}
 mkdir -p grafana/data
 mkdir -p alertmanager/{config,data}
+#Copy configuration file for alertmanager
+cp alertmanager/config/alertmanager-example.yml alertmanager/config/alertmanager.yml
+#Run the containers
 bash docker-container.sh
